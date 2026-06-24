417e1e79ed8d8b188b93fd9e53f9187150049e89
flugel <channelsultan@gmail.com>
2026-06-24 10:50:43 +0500

    Add: number-of-visible-people-in-a-queue - submission-4

---
+++ Data Structures & Algorithms/number-of-visible-people-in-a-queue/submission-4.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        ans = [0] * len(heights)
+        been = []
+        stack = deque()
+        for i in range(len(heights) -1, -1, -1):
+            cur = heights[i]
+            while stack and heights[stack[0]] < cur:
+                ans[i] += 1
+                stack.popleft()
+            
+            if stack:
+                ans[i] += 1 
+
+            stack.appendleft(i)
+        return ans
+        
+            
+
+       
+
+
+        
