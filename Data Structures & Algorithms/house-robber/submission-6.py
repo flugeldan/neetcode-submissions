92f50640adc84e984f05cbea0d4afe8d9fbf3cdc
flugel <channelsultan@gmail.com>
2026-06-25 15:22:08 +0500

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,38 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        memo = {}
+        ans = 0
+        def dfs(i):
+            nonlocal memo
+            nonlocal ans
+            localMaxMemo = nums[i]
+            if i + 1 == len(nums) - 1 or i == len(nums) - 1:
+                ans = max(ans, localMaxMemo)
+                return localMaxMemo
+            for j in range(i + 2, len(nums)):
+                if j not in memo:
+                    memo[j] = dfs(j)
+                localMaxMemo = max(localMaxMemo, memo[j] + nums[i])
+            ans = max(ans, localMaxMemo)
+            return localMaxMemo 
+        ans = 0
+        dfs(0)
+        dfs(1)
+            
+        return ans
+
+
+
+
+
+
+    
+
+
+
+
+
+
+        
