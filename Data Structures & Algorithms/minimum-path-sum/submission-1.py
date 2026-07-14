4ad0fca2365c6f1e855021f0d288e96360395797
flugel <channelsultan@gmail.com>
2026-07-14 15:31:20 +0500

    Add: minimum-path-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-1.py
@@ -0,0 +1,14 @@
+from collections import deque
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        dp = [[float('inf')] * (cols + 1) for _ in range(rows + 1) ]
+        dp[rows - 1][cols] = 0
+
+
+        for i in range(rows - 1, -1, -1):
+            for j in range(cols - 1, -1, -1):
+                dp[i][j] = grid[i][j] + min(dp[i][j + 1], dp[i + 1][j])
+        return dp[0][0]
+                
+
